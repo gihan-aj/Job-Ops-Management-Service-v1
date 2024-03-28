c50153c0784ac5e50adcbdb58746be91ca5b1621
gihan-aj <gihanakila16@gmail.com>
2024-03-29 01:09:41 +0530

    Added entities employee, jobtitle, department

---
+++ JobOps.Domain/Entities/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobOps.Domain.Entities
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
---
+++ JobOps.Domain/Entities/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobOps.Domain.Entities
+{
+    public class Employee
+    {
+        public required string EmployeeId { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public JobTitle? JobTitle { get; set; }
+        public Department? Department { get; set;}
+    }
+}
